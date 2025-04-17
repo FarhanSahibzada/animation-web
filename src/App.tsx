d1bf529764965aa9321102cbae7b95cc89f78693
Farhan Sahibzada <farhansahabzada3@gmail.com>
2025-04-17 00:13:47 -0700

    work on types

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,18 @@ import { useGSAP } from '@gsap/react'
 
 export default function App() {
 
-  const headingRef = useRef(null)
-  const textRef = useRef(null)
+  const headingRef = useRef<HTMLHeadingElement>(null)
+  const textRef = useRef<HTMLParagraphElement>(null)
 
   useGSAP(() => {
-    // Animate paragraph
+    
     gsap.from(textRef.current, {
       y: 20,
       opacity: 0,
       duration: 2,
     })
 
-    // Animate each character in heading
-    gsap.from(headingRef?.current?.querySelectorAll('span'), {
+    gsap.from(headingRef?.current?.querySelectorAll('span') as NodeListOf<HTMLSpanElement> , {
       y: 40,
       opacity: 0,
       duration: 0.6,
@@ -25,20 +24,20 @@ export default function App() {
     })
   }, [])
 
-  // Heading text to animate
+
   const heading = "Animation Web"
 
   return (
     <div className='flex justify-center items-center min-h-screen overflow-x-hidden'>
       <div className='w-full h-[100vh] flex ps-16 items-center bg-gradient-to-br from-blue-200 via-white to-neutral-300'>
         <div className='flex flex-col gap-4'>
-          <h1 ref={headingRef} className=' text-5xl font-semibold'>
+          <h1 ref={headingRef || null} className=' text-5xl font-semibold'>
             {
               heading.split('').map((char, index) => (
                 <span
                   key={index}
                   className='inline-block bg-clip-text text-transparent bg-gradient-to-b from-slate-100 to-neutral-400'
-                  style={{ whiteSpace: 'pre' }} // keeps spaces visible
+                  style={{ whiteSpace: 'pre' }} 
                 >
                   {char}
                 </span>
