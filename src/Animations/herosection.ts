5993bf67e0ee87c2c3a36b63a3131886a7cd3317
Farhan Sahibzada <farhansahabzada3@gmail.com>
2025-04-19 05:55:06 -0700

    more animations

---
+++ src/Animations/herosection.ts
@@ -0,0 +1,41 @@
+import { useGSAP } from "@gsap/react"
+import { RefObject } from "react"
+import gsap from "gsap"
+
+export const textAnimation = (textRef : RefObject<HTMLParagraphElement | null>) => {
+
+    useGSAP(() => {
+        if(textRef.current){
+            gsap.from(
+                textRef.current,
+                {
+                    y: 100,
+                    opacity: 0,
+                    delay: 0.5,
+                    duration : 0.5
+                }
+            )
+        }
+
+    }, [])
+
+}
+
+export const HeadingAnimation = (headingRef : RefObject<HTMLHeadingElement | null>) => {
+
+    useGSAP(() => {
+        if(headingRef.current){
+        gsap.from(
+                headingRef?.current?.querySelectorAll('span') as NodeListOf<HTMLSpanElement>,
+                {
+                    y: 40,
+                    opacity: 0,
+                    duration: 0.6,
+                    stagger: 0.05,
+                    ease: "power2.out",
+                }
+            )
+        }
+    }, [])
+
+}
--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,57 @@
-import React, { useRef } from 'react'
-import gsap from 'gsap'
-import { useGSAP } from '@gsap/react'
+import { useRef } from 'react'
+import { HeadingAnimation, textAnimation } from './Animations/herosection'
+import { motion } from 'framer-motion'
 
 export default function App() {
 
-  const headingRef = useRef<HTMLHeadingElement>(null)
   const textRef = useRef<HTMLParagraphElement>(null)
 
-  useGSAP(() => {
-    
-    gsap.from(textRef.current, {
-      y: 20,
-      opacity: 0,
-      duration: 2,
-    })
-
-    gsap.from(headingRef?.current?.querySelectorAll('span') as NodeListOf<HTMLSpanElement> , {
-      y: 40,
-      opacity: 0,
-      duration: 0.6,
-      stagger: 0.05,
-      ease: "power2.out",
-    })
-  }, [])
-
+  textAnimation(textRef)
 
   const heading = "Animation Web"
+  const words = heading.split("");
+
 
   return (
-    <div className='flex justify-center items-center min-h-screen overflow-x-hidden'>
-      <div className='w-full h-[100vh] flex ps-16 items-center bg-gradient-to-br from-blue-200 via-white to-neutral-300'>
-        <div className='flex flex-col gap-4'>
-          <h1 ref={headingRef || null} className=' text-5xl font-semibold'>
-            {
-              heading.split('').map((char, index) => (
-                <span
-                  key={index}
-                  className='inline-block bg-clip-text text-transparent bg-gradient-to-b from-slate-100 to-neutral-400'
-                  style={{ whiteSpace: 'pre' }} 
-                >
-                  {char}
+    <>
+      <div className='flex justify-center items-center min-h-screen overflow-x-hidden'>
+        <div className='w-full h-[100vh] flex ps-16 items-center bg-gradient-to-br from-blue-200 via-white to-neutral-300'>
+          <div className='flex flex-col gap-4'>
+            <h1 className="text-xl sm:text-2xl md:text-6xl font-bold  tracking-tighter">
+              {words.map((word, wordIndex) => (
+                <span key={wordIndex} className="inline-block mr-1 last:mr-0">
+                  {word.split("").map((letter, letterIndex) => (
+                    <motion.span
+                      key={`${wordIndex}-${letterIndex}`}
+                      initial={{ y: 100, opacity: 0 }}
+                      animate={{ y: 0, opacity: 1 }}
+                      transition={{
+                        delay: wordIndex * 0.1 + letterIndex * 0.03,
+                        type: "spring",
+                        stiffness: 100,
+                        damping: 10,
+                      }}
+                      className="inline-block text-transparent bg-clip-text 
+                    bg-gradient-to-r from-neutral-900 to-neutral-700/80 
+                    dark:from-white dark:to-white/80"
+                    >
+                      {letter}
+                    </motion.span>
+                  ))}
                 </span>
-              ))
-            }
-          </h1>
-          <p ref={textRef} className='text-neutral-800/[0.4] text-lg font-semibold'>
-            this is animation web to learn amazing Animations
-          </p>
+              ))}
+            </h1>
+            <p ref={textRef} className='text-neutral-800/[0.4] text-lg font-semibold'>
+              this is animation web to learn amazing Animations
+            </p>
+          </div>
         </div>
       </div>
-    </div>
+
+      <div style={{ position: 'relative', overflow: 'hidden' }}>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#0099ff" fill-opacity="1" d="M0,192L48,181.3C96,171,192,149,288,117.3C384,85,480,43,576,53.3C672,64,768,128,864,128C960,128,1056,64,1152,53.3C1248,43,1344,85,1392,106.7L1440,128L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path></svg>
+      </div>
+
+    </>
   )
 }
